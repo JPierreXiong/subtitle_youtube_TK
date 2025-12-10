cc3aa865987564b19516a80c54c4c77edf5547c7
JPierreXiong <xiong.jp.cn@gmail.com>
2025-12-10 19:52:07 +0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// pages/index.js
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello Vercel! Deployment Successful!</h1>
+      <p>This is the starting page.</p>
+    </div>
+  );
+}
